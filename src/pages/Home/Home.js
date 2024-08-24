0e31ec4a58fcde1c8acb51b267790e06c7bbcb2d
Muhammad Ahmad Saeed <ahmadsaeed3220@gmail.com>
2024-08-24 18:24:29 +0500

    Added hero section

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,13 +1,32 @@
-import React from 'react'
-import './Home.css'
-import Navbar from '../../components/Navbar/Navbar'
+import React from "react";
+import "./Home.css";
+import Navbar from "../../components/Navbar/Navbar";
+import hero_banner from "../../assets/hero_banner.jpg";
+import hero_title from "../../assets/hero_title.png";
+import play_icon from "../../assets/play_icon.png";
+import info_icon from "../../assets/info_icon.png";
 
 const Home = () => {
   return (
-      <div>
-          <Navbar />
+    <div className="home">
+      <Navbar />
+      <div className="hero">
+        <img src={hero_banner} alt="hero-banner" className="banner-img" />
+        <div className="hero-caption">
+          <img src={hero_title} alt="hero-title" className="caption-img" />
+          <p>
+            Discovering his ties to an ancient secret order, a young man living
+            in modern Istanbul embarks on a quest to save the city from an
+            immortal enemy.
+          </p>
+          <div className="hero-buttons">
+            <button className="btn"> <img src={play_icon} />Play</button>
+            <button className="btn btn-dark" > <img src={info_icon} />More info</button>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
