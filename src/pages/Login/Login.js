b40b5f8b0d93bcedd8221ab01cd97b53f1b9e825
Muhammad Ahmad Saeed <ahmadsaeed3220@gmail.com>
2024-08-27 19:39:35 +0500

    Completed Sign In/Up page

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,26 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.css";
 import logo from "../../assets/logo.png";
 
 const Login = () => {
+
+  const[signIn, setSignIn] = useState("Sign In")
+
   return (
     <div className="login">
       <img src={logo} alt="Netflix" className="login-logo" />
       <div className="login-form">
-        <h1>Sign In</h1>
+        <h1>{signIn}</h1>
         <form>
-          <input type="text" placeholder="Your name" />
+          {signIn === "Sign Up" ? (
+            <input type="text" placeholder="Your name" />
+          ) : (
+            <> </>
+          )}
+
           <input type="email" placeholder="Email" />
           <input type="password" placeholder="Password" />
-          <button>Sign In</button>
+          <button>{signIn}</button>
           <div className="form-help">
-            <div className="remeber">
+            <div className="remember">
               <input type="checkbox" />
-              <label>Remeber Me</label>
+              <label>Remember Me</label>
             </div>
             <p>Need help?</p>
           </div>
         </form>
+        <div className="form-switch">
+          {signIn === "Sign In" ? (
+            <p>
+              New to Netflix? <span onClick={() => {setSignIn("Sign Up")}} >Sign Up Now</span>
+            </p>
+          ) : (
+            <p>
+              Already have account? <span onClick={() => {setSignIn("Sign In")}} >Sign In Now</span>
+            </p>
+          )}
+        </div>
       </div>
     </div>
   );
