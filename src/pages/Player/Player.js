865ccd71b584e036ea0a0461a7c0f962d7ccb1ea
Muhammad Ahmad Saeed <ahmadsaeed3220@gmail.com>
2024-08-29 04:41:57 +0500

    created video player page

---
+++ src/pages/Player/Player.js
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from "react";
+import "./Player.css";
+import back_arrow from "../../assets/back_arrow_icon.png";
+import { useParams } from "react-router-dom";
+
+const Player = () => {
+
+    const { id } = useParams();
+  const [apiData, setApidata] = useState({
+    name: "",
+    key: "",
+    published_at: "",
+    type: "",
+  });
+
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0NTE2Zjg2NTk4NDJhM2M5YjFlZWQ5YTYxNmU1YWNjNSIsIm5iZiI6MTcyNDg4NzQ1MC44OTkwMzUsInN1YiI6IjY2Y2RlNWNlNTczNmFmYmZiMTlkYjIyNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.36oDAf4h8tVVQmjrLrqMQDABS6qsyLusgbqIZ9F-uHY",
+    },
+  };
+
+  useEffect(() => {
+    fetch(
+      `https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`,
+      options
+    )
+      .then((response) => response.json())
+      .then((response) => setApidata(response.results[0]))
+      .catch((err) => console.error(err));
+  }, []);
+
+  return (
+    <div className="player">
+      <img src={back_arrow} alt="back" />
+      <iframe
+        width="90%"
+        height="90%"
+        src={`https://www.youtube.com/embed/${apiData.key}`}
+        title="trailer"
+        frameBorder="0"
+        allowFullScreen
+      ></iframe>
+      <div className="player-info">
+        <p>{apiData.published_at.slice(0,10)}</p>
+        <p>{apiData.name}</p>
+        <p>{apiData.type}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Player;
